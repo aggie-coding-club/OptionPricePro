20f05f5a5c7cbf46b9da0c28e2032ba9fd29ce31
M-postl <mpostl@tamu.edu>
2023-11-07 18:12:11 -0600

    bugged webpage

--- frontend/option-price-pro/src/App.js
+++ frontend/option-price-pro/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+const track = document.getElementById("image-track")
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+window.onmousedown = e => {
+  track.dataset.mouseDownat = e.clientX;
+
+window.onmouseup = () => {
+
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+      maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) *-100;
+      nextPercentage = parseFloat(track.dataset.prevPercentage) +percentage;
+
+  track.dataset.percentage = nextPercentage;
+
+
+  track.animate({
+    transform:  `translate(&{nextPercentage}%, -50%)`
+
+      }, {duration: 1200,fill: "forwards"}    );
+    
+}
+
+for(const image of track.getElementsByClassName('iamge')){
+
+image.animate({
+  objectPosition:  `${nextPercentage} 50%`
+},{duration: 1200,fill:"forwards"}
+
+);
+
+}
+}
 }
 
 export default App;
---
+++ frontend/option-price-pro/src/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return <nav classname="nav">
+        <a href="/" className="site-title">Option Price Pro</a>
+            
+
+
+
+    </nav>    
+}
