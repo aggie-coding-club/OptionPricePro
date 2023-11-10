fdaebee2d076410b0aff91a713b9550e1bac3388
RiceGrainRain <mnmanasnavale@gmail.com>
2023-11-09 18:31:13 -0600

    fixed error - Manas

--- frontend/option-price-pro/src/App.js
+++ frontend/option-price-pro/src/App.js
@@ -1,45 +1,38 @@
-
 import './App.css';
-const track = document.getElementById("image-track")
+const track = document.getElementById("image-track");
 
 function App() {
-window.onmousedown = e => {
-  track.dataset.mouseDownat = e.clientX;
+  window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+  }
 
-window.onmouseup = () => {
+  window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+  }
 
-  track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
-
-window.onmousemove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+  window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
       maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) *-100;
-      nextPercentage = parseFloat(track.dataset.prevPercentage) +percentage;
 
-  track.dataset.percentage = nextPercentage;
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
+    track.dataset.percentage = nextPercentage;
 
-  track.animate({
-    transform:  `translate(&{nextPercentage}%, -50%)`
+    track.animate(
+      { transform: `translate(${nextPercentage}%, -50%)` },
+      { duration: 1200, fill: "forwards" }
+    );
 
-      }, {duration: 1200,fill: "forwards"}    );
-    
-}
-
-for(const image of track.getElementsByClassName('iamge')){
-
-image.animate({
-  objectPosition:  `${nextPercentage} 50%`
-},{duration: 1200,fill:"forwards"}
-
-);
-
-}
-}
+    for (const image of track.getElementsByClassName('image')) {
+      image.animate(
+        { objectPosition: `${nextPercentage}% 50%` },
+        { duration: 1200, fill: "forwards" }
+      );
+    }
+  }
 }
 
 export default App;
