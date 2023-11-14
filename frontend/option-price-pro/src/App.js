95bdb43099b214daf3f4d376ceb954e3c1cb4fe1
RiceGrainRain <mnmanasnavale@gmail.com>
2023-11-14 15:37:06 -0600

    Manas fix error

--- frontend/option-price-pro/src/App.js
+++ frontend/option-price-pro/src/App.js
@@ -1,38 +1,43 @@
-import './App.css';
 const track = document.getElementById("image-track");
 
-function App() {
-  window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-  }
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-  window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-  }
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
 
-  window.onmousemove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-      maxDelta = window.innerWidth / 2;
-
-    const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
-    track.dataset.percentage = nextPercentage;
-
-    track.animate(
-      { transform: `translate(${nextPercentage}%, -50%)` },
-      { duration: 1200, fill: "forwards" }
-    );
-
-    for (const image of track.getElementsByClassName('image')) {
-      image.animate(
-        { objectPosition: `${nextPercentage}% 50%` },
-        { duration: 1200, fill: "forwards" }
-      );
-    }
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
   }
 }
 
-export default App;
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
