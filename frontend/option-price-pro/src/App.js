e28a7024f944bfd75ecea6a46c5d1700708f954f
RiceGrainRain <mnmanasnavale@gmail.com>
2023-11-16 23:55:51 -0600

    fixed image select

--- frontend/option-price-pro/src/App.js
+++ frontend/option-price-pro/src/App.js
@@ -1,34 +1,47 @@
 const track = document.getElementById("image-track");
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => {
+  e.preventDefault(); // Prevent default behavior to avoid text selection
+  track.dataset.mouseDownAt = e.clientX;
+};
 
 const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
+  track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
-}
+};
 
 const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
+  if (track.dataset.mouseDownAt === "0") return;
+
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
+    maxDelta = window.innerWidth / 2;
+
+  const percentage =
+    (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained =
+      parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
   track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
+
+  track.animate(
+    {
+      transform: `translate(${nextPercentage}%, -50%)`
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.style.userSelect = "none"; 
+    image.style.WebkitTouchCallout = "none"; 
+    image.animate(
+      {
+        objectPosition: `${100 + nextPercentage}% center`
+      },
+      { duration: 1200, fill: "forwards" }
+    );
   }
-}
+};
 
 window.onmousedown = e => handleOnDown(e);
 
